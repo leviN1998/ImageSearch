6880a2cb80eb565c227eba310c566721e0e4ad31
KOLMAR Levin <levin.kolmar@somfy.com>
2021-12-07 11:20:58 +0100

    added Docstrings for __init__.py and crawling_base.py

--- ImageCrawling/__init__.py
+++ ImageCrawling/__init__.py
@@ -1,14 +1,69 @@
+""" Module for crawling images from different Websites using
+	a set of Keywords
+	At the Moment only Google with BeautifulSoup is supported,
+	which leads to ~80 Images per Keyword
+
+Imports:
+--------
+    crawl_soup file with the BeautifulSoup-Image Crawler. Implements _get_image_urls to get urls of the images from google
+    crawling_base provides functions for creating folders and saving images locally
+    os is needed to get the current working directory's path
+
+Functions:
+----------
+download_images(query, image_count, folder, verbose): str, int, str, bool -> None
+	downloads images to a Keyword (query) into a specific folder (folder)
+	path is relative to working directory
+
+download_images_batch(): -> None
+	optimized version of download_images to support using more than one Keyword
+
+TODOS:
+------
+    TODO: implemet download_images_batch()
+    TODO: add support for Selenium
+    TODO: add Crawler for Google with Selenium
+    TODO: add Crawler for Flickr with Selenium
+    TODO: change Filestructure of Image-Databases
+    TODO: add functionality to save images into Database (binary vs. path ??)
+    TODO: add support for Feature Extraction
+    TODO: add functionality of using 5 example images to measure how good results are
+
+INFORMATION:
+------------
+	Every scraper needs to have a function _get_image_urls(query, image_count, verbose)
+	That collects all image urls on that website
+
+"""
 from .crawl_soup import *
 from .crawling_base import *
 import os
 
 
-"""
-Every scraper needs to have a function _get_image_urls(query, image_count, verbose)
-That collects all image urls on that website
 
-"""
 def download_images(query: str, image_count: int = 0, folder: str = "", verbose: bool = True):
+	""" downloads Images for a given Keyword into a given Folder
+
+    Parameters:
+    -----------
+    query: str
+    	Keyword String to enter into website for searching the images
+
+    image_count: int, optional
+    	Desired Number of images, defaults to 0 which means as many as the crawler can get
+
+    folder: str, optional
+    	Path to folder where images should be saved, defaults to "" which means current working directory
+    	Paths are relative to current working directory
+
+    verbose: bool, optional
+    	Flag whether this function and functions that are called should output information to the console
+    	defaults to True
+
+    Returns:
+    --------
+    Nothing
+    """
     old_pwd = os.getcwd()
     if not crawling_base._change_folder(folder, verbose):
         return
@@ -18,4 +73,6 @@ def download_images(query: str, image_count: int = 0, folder: str = "", verbose:
     os.chdir(old_pwd)
 
 def download_images_batch():
+	""" TODO: implement
+	"""
     pass
--- ImageCrawling/crawling_base.py
+++ ImageCrawling/crawling_base.py
@@ -1,9 +1,45 @@
+""" File to privide basic functionality needed to save images
+    and handle folder operations
+
+Imports:
+--------
+    requests needed to use http requests to download image referred by a url
+    os needed for creating folders and saving images
+
+Functions:
+----------
+_change_folder(folder_name, verbose): str, bool -> bool
+    creates folder with given path and changes into it
+    if folder already exists only changes into it
+    Throws exeption if anything goes wrong
+
+_save_images(urls, name, image_count, verbose): [str], str, int, bool -> Nothing
+    saves a list of images as given name with an index added
+
+
+TODOS:
+------
+    see __init__.py
+"""
 import requests
 import os
 
 
 def _change_folder(folder_name: str, verbose: bool) -> bool:
-    """
+    """ changes working directory to desired folder and creates this folder if needed
+        returns False if folder was not created
+
+    Parameters:
+    -----------
+    folder_name: str
+        realtive path of folder to switch to
+
+    verbose: bool
+        Flag whether this function and functions that are called should output information to the console
+
+    Returns:
+    --------
+    bool: Was this operation succesfull?
     """
     folder_path = os.path.join(os.getcwd(), folder_name)
     if folder_name != "" and not os.path.exists(folder_path):
@@ -28,7 +64,26 @@ def _change_folder(folder_name: str, verbose: bool) -> bool:
 
 
 def _save_images(urls, name: str, image_count: int, verbose: bool):
-    """
+    """ saves images referred by a list of urls into File-System
+
+    Parameters:
+    -----------
+    urls: [str]
+        List of Image-Urls to save
+
+    name: str
+        name which generated files should have
+
+    image_count: int
+        how many images should originally be crawled
+        used for outputting information
+
+    verbose: bool
+        Flag whether this function and functions that are called should output information to the console
+
+    Returns:
+    --------
+    Nothing
     """
     count = 0
     for link in urls:
