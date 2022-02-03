e7c27258b8e8a89ef28ff28125120ca16d393dc9
leviN1998 <levin.kolmar@googlemail.com>
2022-02-03 23:57:21 +0100

    adding all networks 5/5

--- test_levin.py
+++ test_levin.py
@@ -66,7 +66,7 @@ if __name__ == '__main__':
     # ---------------- Am Server ausführen
     # TODO: schon gecralte Keywords nicht mehr crawlen
     # ImageCrawling.crawl_from_txt("ImageDatabases/keywords.txt", "final.db", "big", 2200, 200, main_threads=1, child_threads=1)
-    ImageCrawling.calculate_all_features("final.db", count=1000)
+    ImageCrawling.calculate_all_features("final.db", count=0)
 
 
     ## -------------- Testing andere Netze
