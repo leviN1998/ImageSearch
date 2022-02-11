63a5a7e5f1c0921d9dc732589c32731d5ae194be
leviN1998 <levin.kolmar@googlemail.com>
2022-02-11 08:59:52 +0100

    pyServer update

--- pyServer.py
+++ pyServer.py
@@ -28,7 +28,7 @@ def recv():
         print("client with address: ", addr, " is connected.")
         data = conn.recv(104857600) # 100 MB
         # data = data.decode("utf-8")
-        data = data.decode("unicode")
+        data = data.decode("unicode-escape")
         print("Recieved this data: <", data[0:30], "> from the client.")
 
         data_recieved = data.split()
