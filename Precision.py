4532f93af0d84785fc19e2c6f30df4b88a385928
doyouevershine0329 <19970329maydaycxh@gmail.com>
2022-02-20 14:20:26 +0100

    precision

---
+++ Precision.py
@@ -0,0 +1,34 @@
+import ImageCrawling
+import numpy as np
+import io
+import statistics
+from ImageCrawling import database_tools
+
+# search precision
+def precision(database: str, network: str):
+    keywords = ImageCrawling.get_keywords(database)
+    print(keywords)
+    precision_for_keyword = []
+    precision_for_all = []
+
+    for k in keywords:
+        test_bi_feas = database_tools.get_test_images_for_class(database, network, "big", k)[:10]
+        test_feas = np.load(io.BytesIO(test_bi_feas))
+        for fea in test_feas:
+            # feature = extractors.MobileNet.extractImage(img)
+            classes = ImageCrawling.search_for_classes(database, "big", network, fea, count=50)
+            count = 0
+            for c in classes:
+                if k == c:
+                    count += 1
+            pre = count/50
+            # print(pre)
+            precision_for_keyword.append(pre)
+        avg_pre = sum(precision_for_keyword)/10
+        print("precision for class:" + avg_pre)
+        precision_for_all.append(avg_pre)
+    return statistics.mean(precision_for_all)
+
+
+if __name__ == "__main__":
+    print(precision("final.db", ImageCrawling.mobileNet))
